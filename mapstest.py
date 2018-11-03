d7d1281d841f87c392a111290b3edd39292cb7a8
Jenny Wang <jenny.wang244@gmail.com>
2018-11-03 14:07:17 -0700

    Efficiency?

--- mapstest.py
+++ mapstest.py
@@ -1,33 +1,47 @@
 import googlemaps
-from my_token import my_access_token
+from access_token import token
+token = token()
+import time
 import random
 
-gmaps = googlemaps.Client(key=my_access_token)
-places = gmaps.places_nearby(location=(37.870569,-122.251584), radius=1500)
-place_types = [p['types'] for p in places['results']]
+start_time = time.time()
+gmaps = googlemaps.Client(key=token)
 
-#combine types
 type_counts = {}
-for place in place_types:
-    for t in place:
-        type_counts[t] = type_counts.get(t, 0) + 1
+radius = 1500
+location = (34.876615, -112.916040)
 
-# Choose at most 3 random types
-if len(type_counts.keys()) > 3:
-    choices = []
-    [choices.extend([[key]*type_counts[key] for key in type_counts.keys()])]
-    choices = sum(choices, [])
+# Expand the searching area's radius by 2 until 
+# there are at least 3 type of landmarks
+tries = 0
+while len(type_counts) == 0 or (len(type_counts) < 3 and tries < 5 and start_time + 5 > time.time()): 
+    type_counts = {}
+    places = gmaps.places_nearby(location=location, radius=radius)
+    place_types = sum([p['types'] for p in places['results']],[])
 
-    choice = []
-    while len(choice) < 3:
-        new_choice = random.choice(choices)
-        if new_choice not in choice:
-            choice.append(new_choice)
-else:
-    choice = list(type_counts.keys())
+    # Count total instances of each type
+    type_counts = {}
+    for ty in place_types:
+        type_counts[ty] = type_counts.get(ty, 0) + 1
+    radius *= 3
+    tries += 1
+
+# Choose three random types (weighted by frequency)
+choices = []
+choices = sum([[key]*type_counts[key] for key in type_counts.keys()], [])
+
+# Make sure the final choices aren't the same
+choice = []
+tries = 0
+while len(choice) < 3 and tries < 50:
+    new_choice = random.choice(choices)
+    if new_choice not in choice:
+        choice.append(new_choice)
+    tries += 1
 
 # Use choices to pick words of inspiration~~~
 from type_word_dictionary import type_word_dict
 inspiration = [random.choice(type_word_dict.get(c, ['Not supported yet'])) for c in choice]
 
 print(inspiration)
+print(choice)

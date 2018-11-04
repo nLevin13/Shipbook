e70e9205d2e51133950a87fb5b320f50645c5b7a
Jenny Wang <jenny.wang244@gmail.com>
2018-11-04 01:26:45 -0700

    Add service for static resources

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 /*
 const http = require('http');
-
 const hostname = '127.0.0.1';
 const port = 8000;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Henlo World\n');
 });
-
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
@@ -17,10 +14,13 @@ server.listen(port, hostname, () => {
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var express = require('express')
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static('public'));
 
 io.on('connection', function(socket){
   console.log('a user connecc');
